f61e8d88510972d768d4d640c75ae25e5ab0c8bf
Artem <k.artem@netassist.ua>
2016-08-25 15:14:26 +0300

    add db_.php

---
+++ basic/config/db_.php
@@ -0,0 +1,13 @@
+
+<?php
+/*
+ * Для работы приложения переименовать файл с db_.php в db.php и вставить данные подключения
+ */
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=193.104.254.29;dbname=alfa_db',
+    'username' => 'mon',
+    'password' => 'dfjhs7kff223hl',
+    'charset' => 'utf8',
+];
