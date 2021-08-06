bebfe9e137833660da6d7e61b38d5a527280d4a6
NetassistArtem <k.artem@netassist.ua>
2021-08-06 17:34:07 +0300

    Update db2.php
    delete test data

--- basic/config/db2.php
+++ basic/config/db2.php
@@ -2,8 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=icinga',
-    'username' => 'isinga_user',
-    'password' => '52mGBFsT37Nu5SHc',
+    
+    
     'charset' => 'utf8',
 ];
