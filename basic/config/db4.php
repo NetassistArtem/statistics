7026d06cc9c12344c8c63ca998bf51b3b7a14b05
NetassistArtem <k.artem@netassist.ua>
2021-08-06 17:36:59 +0300

    Update db4.php
    change test data 4

--- basic/config/db4.php
+++ basic/config/db4.php
@@ -2,8 +2,6 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=switch_down_history',
-    'username' => 'switch_down_user',
-    'password' => '74Wmp8rrmRWXqbvz',
+   
     'charset' => 'utf8',
 ];
